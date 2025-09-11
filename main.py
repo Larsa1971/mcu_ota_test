cac9441a71e7765decd83f3b7580f2f675f25586
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-11 22:58:36 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import uasyncio as asyncio
+import ota
+
+async def main():
+    # Kolla att koden fungerar, annars rollback
+    ota.rollback_if_broken()
+
+    # Starta OTA-worker
+    asyncio.create_task(ota.ota_worker())
+
+    # Starta din riktiga applikation
+    import app_main
+    await app_main.run()
+
+try:
+    asyncio.run(main())
+finally:
+    asyncio.new_event_loop()
