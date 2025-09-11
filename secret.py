0197c0d9a2c4001d62f9990d00930294a66e727a
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-11 22:42:24 +0200

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,11 +1,10 @@
-# secret.py
+# WiFi
 WIFI_SSID = "DIN_SSID"
 WIFI_PASSWORD = "DITT_LÖSENORD"
 
-# Token behövs bara om du ska hämta från privat repo
+# GitHub
 GITHUB_TOKEN = "ghp_xxxDIN_TOKENxxx"
-
-# URL till manifest.json i ditt repo
-# OBS: använd GitHub API-länk, inte raw.githubusercontent.com
-# Exempel: "https://api.github.com/repos/<USER>/<REPO>/contents/manifest.json?ref=main"
-MANIFEST_URL = "https://api.github.com/repos/<USER>/<REPO>/contents/manifest.json?ref=main"
+USER = "dittgithubnamn"
+REPO = "dittrepo"
+BRANCH = "main"
+FILE_PATH = "main.py"   # filen vi vill hålla uppdaterad
