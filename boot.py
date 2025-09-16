e1cb9152efb20d7bb6e409eb16d1ded1593e5a5c
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-16 22:26:20 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,6 +1,8 @@
+import machine
 import network
-import time
 import secret
+import time
+import os
 
 def wifi_connect(ssid, password, timeout=15):
     wlan = network.WLAN(network.STA_IF)
@@ -17,5 +19,18 @@ def wifi_connect(ssid, password, timeout=15):
 try:
     ifconfig = wifi_connect(secret.WIFI_SSID, secret.WIFI_PASSWORD)
     print("WiFi ansluten:", ifconfig)
+    
+    if "app_main_old.py" in os.listdir() and "app_main.py" in os.listdir():
+        try:
+            with open("app_main.py") as f:
+                compile(f.read(), "app_main.py", "exec")
+            print("Fungerande app_main.py!")
+        except Exception as e2:
+            print("Fel i app_main.py – gör rollback")
+            os.remove("app_main.py")
+            os.rename("app_main_old.py", "main.py")
+            await asyncio.sleep(1)
+            machine.reset()
+    
 except Exception as e:
     print("WiFi-anslutning misslyckades:", e)
