47ece824720b02ab3251ed64310d965a286fa8b1
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-16 22:41:02 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -25,10 +25,13 @@ try:
             with open("app_main.py") as f:
                 compile(f.read(), "app_main.py", "exec")
             print("Fungerande app_main.py!")
+            if "app_main_old.py" in os.listdir():
+                os.remove("app_main_old.py")
+
         except Exception as e2:
             print("Fel i app_main.py – gör rollback")
             os.remove("app_main.py")
-            os.rename("app_main_old.py", "main.py")
+            os.rename("app_main_old.py", "app_main.py")
             time.sleep(1)
             machine.reset()
     
