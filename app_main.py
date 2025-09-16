3bf679e5e50bd1cd438146392d52e69e89f6d7f0
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-16 22:31:44 +0200

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -6,9 +6,9 @@ led = machine.Pin("LED", machine.Pin.OUT)
 async def blink_task():
     while True:
         led.value(1)
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.5)
         led.value(0)
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.5)
 
 async def run():
     asyncio.create_task(blink_task())
