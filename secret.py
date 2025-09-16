1ed9259ecdb94fa52ded543678c5d6ed6776b88c
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-09-16 23:34:04 +0200

    Update secret.py

--- secret.py
+++ secret.py
@@ -8,4 +8,4 @@ USER = "dittgithubnamn"
 REPO = "dittrepo"
 BRANCH = "main"
 FILE_PATH = "app_main.py"   # filen vi vill hålla uppdaterad
-CHECK_INTERVAL = 120 # tiden i sekunder för verifiering om ny version kommit
+CHECK_INTERVAL = 43200 # 1800 = 30 min 43 200 = 12 tim tiden i sekunder för verifiering om ny version kommit
